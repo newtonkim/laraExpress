50e024424158bca3643d79186b44ef5bb275446e
newtonkim <knewts98@gmail.com>
2022-03-06 10:20:54 +0300

    work on Menu module

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MenuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->paragraphs(2, true),
+            'price' => $this->faker->numberBetween(99, 999),
+        ];
+    }
+}
--- database/seeders/MenusTableSeeder.php
+++ database/seeders/MenusTableSeeder.php
@@ -2,18 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Menu;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class MenusTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
         \App\Models\Menu::factory(10)->create();
     }
 }
